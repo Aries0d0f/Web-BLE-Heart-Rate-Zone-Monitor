f74f763a52a4ca0235135b539e158cfc87aa2fed
aries0d0f <aries0d0f@gmail.com>
2025-01-25 17:17:32 +0800

    build: Setup PWA for Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,28 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { VitePWA } from "vite-plugin-pwa";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue()
+    vue(),
+    VitePWA({
+      injectRegister: "script-defer",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        navigateFallback: "index.html",
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff,woff2}"],
+        globIgnores: ["assets/images/favicon/*"],
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+      manifest: {
+        name: "Web BLE Heart Rate Zone Monitor",
+        theme_color: "#000000"
+      },
+    }),
   ],
   resolve: {
     alias: {
