1950976684b316c419b4fee7fce8325490201d9e
aries0d0f <aries0d0f@gmail.com>
2025-01-25 22:34:10 +0800

    feat(module): Make feature sets constraint.

--- src/modules/bluetooth/bluetooth.model.ts
+++ src/modules/bluetooth/bluetooth.model.ts
@@ -1,11 +1,12 @@
 export const Features = {
   HEART_RATE: {
+    ID: 'HEART_RATE',
     Name: 'Heart Rate',
     Icon: 'fa6-solid:heart-pulse',
     Service: 'heart_rate',
     Characteristic: 'heart_rate_measurement',
   }
-}
+} as const;
 
 export type Feature = keyof typeof Features;
 
