e011e5967128ac9f7bbf3a1f7ba6ee8320868cad
aries0d0f <aries0d0f@gmail.com>
2025-01-25 18:21:47 +0800

    feat(style): Addd RWD.

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,32 @@ setupModules();
 .viewport {
   &-container {
     --wrapper-gap: 5em;
+    --monitor-width: 10em;
 
     background-color: var(--color-black);
     height: 100%;
     width: 100%;
     place-content: center;
     place-items: center;
+    font-size: 32px;
+    will-change: font-size;
+    transition: font-size 0.5s ease-in-out;
+
+    @media screen and (max-width: 1680px) {
+      font-size: 24px;
+    }
+
+    @media screen and (max-width: 1400px) {
+      font-size: 20px;
+    }
+
+    @media screen and (max-width: 1024px) {
+      font-size: 18px;
+    }
+
+    @media screen and (max-width: 768px) {
+      font-size: 16px;
+    }
   }
   
   &-wrapper {
