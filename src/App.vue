b1e0f8a5cf7c1cadac90cdb37ccf52caf0acb52a
aries0d0f <aries0d0f@gmail.com>
2025-01-25 17:19:00 +0800

    feat(view): Attach components to app entry.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <script setup lang="ts">
+import { setupModules } from '@/modules';
+import Controller from '@/components/Controller.vue';
+import Monitor from './components/Monitor.vue';
 
+setupModules();
 </script>
 
 <template>
-
+  <main :class="[$style['viewport-container'], $style.wrapper, $style.flex, $style.vertical]">
+    <div :class="[$style['viewport-wrapper'], $style.wrapper, $style.flex, $style.horizontal]">
+      <Monitor feature="HEART_RATE" />
+    </div>
+    <Controller />
+  </main>
 </template>
 
 <style module scoped lang="scss">
+@use '@/assets/styles/modules/wrapper.module' as wrapper;
 
+.viewport {
+  &-container {
+    --wrapper-gap: 5em;
+
+    background-color: var(--color-black);
+    height: 100%;
+    width: 100%;
+    place-content: center;
+    place-items: center;
+  }
+  
+  &-wrapper {
+    --wrapper-gap: 2em;
+  }
+}
 </style>
